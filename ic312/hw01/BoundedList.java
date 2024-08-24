6daa07a60e7c02b422d41bf2c0f55732dc44b031
NSchmidt <m265646@usna.edu>
2024-08-23 22:41:44 -0400

    new commit

--- ic312/hw01/BoundedList.java
+++ ic312/hw01/BoundedList.java
@@ -7,7 +7,8 @@ import java.util.*;
 
 public class BoundedList<T> implements List<T> {
   private T[] elements;
-  // TODO you might want another private field...
+  private int size = 0;
+  private int capacity;
 
   /** Create a new BoundedList with the given maximum capacity.
    */
@@ -15,31 +16,83 @@ public class BoundedList<T> implements List<T> {
     @SuppressWarnings("unchecked")
     T[] elements = (T[]) new Object[capacity];
     this.elements = elements;
+    this.capacity = capacity;
   }
 
   @Override
   public T get(int index) throws IndexOutOfBoundsException {
-    throw new UnsupportedOperationException("DELETE THIS!");
+    if (index < 0 || index >= size) {
+    throw new IndexOutOfBoundsException("outside of current list size");
+    }
+    else {
+      return elements[index];
+    }
   }
 
   @Override
   public void set(int index, T data) throws IndexOutOfBoundsException {
-    throw new UnsupportedOperationException("DELETE THIS!");
+    if (index < 0 || index >= size) {
+    throw new IndexOutOfBoundsException("outside of current list size");
+    }
+    else {
+      elements[index] = data;
+    }
   }
 
   @Override
   public void add(int index, T data) throws IndexOutOfBoundsException, IllegalStateException {
-    throw new UnsupportedOperationException("DELETE THIS!");
+    if (size == capacity) {
+      throw new IllegalStateException("list is full");     
+    }
+    else if (index < 0) {
+      throw new IndexOutOfBoundsException("negative index");
+    }
+    else if (index >= capacity) {
+      throw new IndexOutOfBoundsException("outside of bounds of fixedList at size " + capacity);
+    }
+    else if (index > size) {
+      throw new IndexOutOfBoundsException("adding more than +1 the current size");
+    }
+    else {
+      if (size == 0) {
+        elements[0] = data;
+      }
+      else if (index == size) {
+        elements[index] = data;
+      }
+      else {
+        for (int i = size; i > index; i--) {
+          elements[i] = elements[i - 1];
+          if (i == index + 1) {
+            elements[index] = data;
+          }
+        }
+      }
+      size++;
+    }
   }
 
   @Override
   public void remove(int index) throws IndexOutOfBoundsException {
-    throw new UnsupportedOperationException("DELETE THIS!");
+    if (index < 0 || index >= size) {
+      throw new IndexOutOfBoundsException("outside of current list size");
+    }
+    else {
+      for (int i = index; i < size; i++) {
+        if (i + 1 == size) {
+          elements[i] = null;
+        }
+        else {
+          elements[i] = elements[i + 1];
+        }
+      }
+      size--;
+    }
   }
 
   @Override
   public int size() {
-    throw new UnsupportedOperationException("DELETE THIS!");
+    return size;
   }
 
   @Override
@@ -47,7 +100,7 @@ public class BoundedList<T> implements List<T> {
   public String toString() {
     StringBuilder sb = new StringBuilder();
     sb.append("[ ");
-    for (int i = 0; i < size(); ++i) {
+    for (int i = 0; i < size; ++i) {
       sb.append(get(i).toString());
       sb.append(' ');
     }
@@ -57,5 +110,14 @@ public class BoundedList<T> implements List<T> {
   public static void main(String args[]) {
     //Main is not necessary. It is just for me to test my own code
     List<Integer> lst = new BoundedList<Integer>(5);
+    lst.add(0,6);
+    lst.add(1,7);
+    lst.add(2,8);
+    lst.add(1,9);
+    lst.add(0,10);
+    lst.set(0,69);
+        System.out.println(lst.get(2));
+    //System.out.println();
+    System.out.println(lst.toString());
   }
 }
