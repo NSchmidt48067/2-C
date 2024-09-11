651901fa45f06036bd82d6290a8a17d78356a0be
NSchmidt48067 <vm@usna.edum265646@usna.edu>
2024-09-11 09:37:16 -0400

    new commit

--- ic322/lab3/server.py
+++ ic322/lab3/server.py
@@ -3,7 +3,7 @@ from socket import *
 import signal
 import sys
 
-serverPort = 1200
+serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
