59409c3819b316a782e7f383e454eee4d01d1598
NSchmidt48067 <vm@usna.edum265646@usna.edu>
2024-09-12 21:18:33 -0400

    new commit

--- ic322/lab4/helper.py
+++
@@ -1,7 +0,0 @@
-def createH():
-    file = open("data.txt", "r")
-    data = file.read()#Read in entire file
-    d = dict(x.split(":") for x in data.split("\n"))#Split file into array of strings separated by new lines
-    #Then split up said strings by colons into a dictionary
-    print(d)
-    return d
