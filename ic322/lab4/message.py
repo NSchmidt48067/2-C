22f0d261ba49ff0a21ee2cbb8f944fc07afd8a7c
NSchmidt48067 <vm@usna.edum265646@usna.edu>
2024-09-16 15:22:13 -0400

    new commit

--- ic322/lab4/message.py
+++ ic322/lab4/message.py
@@ -9,23 +9,25 @@ def createH():
     lines = data.strip().split("\n")#split by line
     d = {}
     for line in lines:
-        x = line.strip().split(":", 1)
-        if (x[0] == "Cookie:"):
-            cookies = x[1].strip().split(";")
-            a = cookies.strip().split("=")
-            print(a)
-            d.update(a)
-        d[x[0].strip()] = x[1].strip()
+        key, value = line.split(':', 1)
+        d[key.strip()] = value.strip()
     return d
 
 
 
 url = "https://mids.usna.edu/ITSD/mids/drgwq010$.startup"
 
-header = createH()
+d = createH()
+
+#for i in d:
+ #  print (i, d[i])
+
+header = d
+
+
 
 payload = {
-    'P_ALPHA': input("Enter an alpha: "),
+    'P_ALPHA': '265646',
     'P_LAST_NAME': '',
     'P_MICO_CO_NBR':'', 
     'P_SECOF_COOF_SEBLDA_AC_YR':'2025',
@@ -33,16 +35,17 @@ payload = {
     'P_SECOF_COOF_SEBLDA_BLK_NBR':'1',
     'P_MAJOR_CODE':'',
     'P_NOMI_FORMATTED_NAME':'',
-    'Z_ACTION=QUERY&Z_CHK':'0'
+    'Z_ACTION': 'QUERY',
+    'Z_CHK':'0'
     }
 
 response = requests.post(url, data=payload,headers=header)
 
 print(f'Status Code: {response.status_code}')
 
-parsed = BeautifulSoup(response.content, "html.parser")
+#parsed = BeautifulSoup(response.content, "html.parser")
 
-file = open('file.html', 'w')
-file.write(str(parsed))
+#file = open('file.html', 'w')
+#file.write(str(parsed))
 
-print(parsed)
+#print(parsed)
