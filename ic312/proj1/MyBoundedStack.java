0ee5f9d27f9542c54f5d5496c79a3e2e78894001
NSchmidt <m265646@usna.edu>
2024-09-19 21:49:58 -0400

    new commit

--- ic312/proj1/MyBoundedStack.java
+++ ic312/proj1/MyBoundedStack.java
@@ -114,41 +114,65 @@ public class MyBoundedStack<T> implements BoundedStack<T> {
 
     //move array over backwards and then make it forward
     //i = tail -1 because tail is always off by 1
-    for (int i = (tail - 1 + this.capacity) % this.capacity; count < capacity && elements[i] != null; i = (i - 1 + this.capacity) % this.capacity) {
+    for (int i = (tail - 1 + this.capacity) % this.capacity; count < size && count < capacity; i = (i - 1 + this.capacity) % this.capacity) {
       temp[count] = elements[i];//0 = tail
+      System.out.println(elements[i]);
       count++;
     }
+    count--;//Count is 1 too large
     //Array has been moved over, but is backwards
     //Make it forwards
-    for (int i = 0; i < size; i++) {
-//^wrong; not size
+    int n = count;
+    @SuppressWarnings("unchecked")
+    T[] t2 = (T[]) new Object[capacity];
+    //i <= n because count is the number of times to copy
+    for (int i = 0; i <= n; i++) {
+      t2[i] = temp[count];
       count--;
     }
-
 
     if (size >= capacity) {
-      count = capacity - 1;
-      }
-    else {
-      count = size - 1;
-    }
-    int i = tail;
-    do {
-      i = (i - 1 + this.capacity) % this.capacity;
-      temp[count] = elements[i];
-      count--;
-      if (count == -1)
-        break;
-    }while(i != head);
-
-    if (size >= capacity){
       size = capacity;
+      head = 0;
+      tail = 0;
+      //new array so head = 0
+      //array is full so tail is up by 1 which is 0
     }
-    head = 0;
-    tail = size;
+    else {
+      head = 0;
+      tail = size;
+      //size stays the same
+      //head always = 0
+      //tail = size because size is 1 larger than the position
+    }
+
     this.capacity = capacity;
 
-    elements = temp;
+    elements = t2;
+
+    // if (size >= capacity) {
+    //   count = capacity - 1;
+    //   }
+    // else {
+    //   count = size - 1;
+    // }
+    // int i = tail;
+    // do {
+    //   i = (i - 1 + this.capacity) % this.capacity;
+    //   temp[count] = elements[i];
+    //   count--;
+    //   if (count == -1)
+    //     break;
+    // }while(i != head);
+
+    // if (size >= capacity){
+    //   size = capacity;
+    // }
+    // head = 0;
+    // tail = size;
+    // this.capacity = capacity;
+
+    // elements = temp;
   }
 
   /** Returns whether the stack is currently empty. */
@@ -195,47 +219,48 @@ public class MyBoundedStack<T> implements BoundedStack<T> {
   }
 
   public static void main(String args[]) {
-    // BoundedStack<Integer> stk = new MyBoundedStack<>(3);
-    // stk.push(10);
-    // stk.push(20);
-    //     //System.out.println(stk.get(0));
-    // stk.setCapacity(5);
-    // stk.push(30);
-    // stk.push(40);
-    // // System.out.println(stk.get(0));
-    // // System.out.println(stk.get(1));
-    // // System.out.println(stk.get(2));
-    // // System.out.println(stk.get(3));
+    BoundedStack<Integer> stk = new MyBoundedStack<>(3);
+    stk.push(10);
+    stk.push(20);
+        //System.out.println(stk.get(0));
+    stk.setCapacity(5);
+    stk.push(30);
+    stk.push(40);
+    // System.out.println(stk.get(0));
+    // System.out.println(stk.get(1));
+    // System.out.println(stk.get(2));
+    // System.out.println(stk.get(3));
 
-    // //System.out.println(stk.toString());
+    //System.out.println(stk.toString());
 
-    // if (40 == (int)stk.pop()) {
-    //   System.out.println("success");
-    // }
-    //     if (30 == (int)stk.pop()) {
-    //   System.out.println("success");
-    // }
-    
-    // System.out.println(stk.getAll());
-
-    //     if (20 == (int)stk.pop()) {
-    //   System.out.println("success");
-    // }
-    //     if (10 == (int)stk.pop()) {
-    //   System.out.println("success");
-    // }
-    // stk.push(100);
-    // stk.push(200);
-    // stk.push(300);
-    
-    // stk.setCapacity(1);
-    // if (!stk.isEmpty()) {
-    //   System.out.println("not empty");
-    // }
-    // if (300 == (int)stk.pop()) {
-    //   System.out.println("success");
-    // }    
-    // if (stk.isEmpty()) {
-    //   System.out.println("empty");
+    if (40 == (int)stk.pop()) {
+      System.out.println("success");
     }
+        if (30 == (int)stk.pop()) {
+      System.out.println("success");
+    }
+    
+    System.out.println(stk.getAll());
+
+        if (20 == (int)stk.pop()) {
+      System.out.println("success");
+    }
+        if (10 == (int)stk.pop()) {
+      System.out.println("success");
+    }
+    stk.push(100);
+    stk.push(200);
+    stk.push(300);
+    
+    stk.setCapacity(1);
+    if (!stk.isEmpty()) {
+      System.out.println("not empty");
+    }
+    if (300 == (int)stk.pop()) {
+      System.out.println("success");
+    }    
+    if (stk.isEmpty()) {
+      System.out.println("empty");
+    }
+  }
 }
