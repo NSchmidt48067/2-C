f2d0fc6d8c83f32b5233ec5854042968fd8141c6
NSchmidt <m265646@usna.edu>
2024-09-19 22:18:01 -0400

    new commit

--- ic312/proj1/MyBoundedStack.java
+++ ic312/proj1/MyBoundedStack.java
@@ -82,33 +82,35 @@ public class MyBoundedStack<T> implements BoundedStack<T> {
    * removed.
    */
   public void setCapacity(int capacity) {
-    @SuppressWarnings("unchecked")
-    T[] temp = (T[]) new Object[capacity];
-    int count;
-    
-    if (size >= capacity) {
-      count = capacity - 1;
+    if (capacity != 0){
+      @SuppressWarnings("unchecked")
+      T[] temp = (T[]) new Object[capacity];
+      int count;
+      
+      if (size >= capacity) {
+        count = capacity - 1;
+        }
+      else {
+        count = size - 1;
       }
-    else {
-      count = size - 1;
-    }
-    int i = tail;
-    do {
-      i = (i - 1 + this.capacity) % this.capacity;
-      temp[count] = elements[i];
-      count--;
-      if (count == -1)
-        break;
-    }while(i != head);
+      int i = tail;
+      do {
+        i = (i - 1 + this.capacity) % this.capacity;
+        temp[count] = elements[i];
+        count--;
+        if (count == -1)
+          break;
+      }while(i != head);
 
-    if (size >= capacity){
-      size = capacity;
-    }
-    head = 0;
-    tail = size;
-    this.capacity = capacity;
+      if (size >= capacity){
+        size = capacity;
+      }
+      head = 0;
+      tail = size;
+      this.capacity = capacity;
 
-    elements = temp;
+      elements = temp;
+    }
   }
 
   /** Returns whether the stack is currently empty. */
@@ -152,51 +154,5 @@ public class MyBoundedStack<T> implements BoundedStack<T> {
     }
     sb.append(']');
     return sb.toString();
-  }
-
-  public static void main(String args[]) {
-    BoundedStack<Integer> stk = new MyBoundedStack<>(3);
-    stk.push(10);
-    stk.push(20);
-        //System.out.println(stk.get(0));
-    stk.setCapacity(5);
-    stk.push(30);
-    stk.push(40);
-    // System.out.println(stk.get(0));
-    // System.out.println(stk.get(1));
-    // System.out.println(stk.get(2));
-    // System.out.println(stk.get(3));
-
-    //System.out.println(stk.toString());
-
-    if (40 == (int)stk.pop()) {
-      System.out.println("success");
-    }
-        if (30 == (int)stk.pop()) {
-      System.out.println("success");
-    }
-    
-    System.out.println(stk.getAll());
-
-        if (20 == (int)stk.pop()) {
-      System.out.println("success");
-    }
-        if (10 == (int)stk.pop()) {
-      System.out.println("success");
-    }
-    stk.push(100);
-    stk.push(200);
-    stk.push(300);
-    
-    stk.setCapacity(1);
-    if (!stk.isEmpty()) {
-      System.out.println("not empty");
-    }
-    if (300 == (int)stk.pop()) {
-      System.out.println("success");
-    }    
-    if (stk.isEmpty()) {
-      System.out.println("empty");
-    }
   }
 }
