9e2b28bfef0f4eeab2a676883f3dcf75c86b80fb
NSchmidt48067 <vm@usna.edum265646@usna.edu>
2024-09-25 19:52:47 -0400

    new commit

---
+++ ic312/hw05/BSTMap.java
@@ -0,0 +1,61 @@
+import java.util.Deque;
+
+public class BSTMap<K extends Comparable<K>, V> implements Map<K,V> {
+  // private classes and fields here
+
+private class Node {
+    T data;
+    Node next;
+
+    public Node(T data, Node next) {
+      this.data = data;
+      this.next = next;
+    }
+  }
+
+  private Node head = null;
+  private Node tail = null;
+  private int size = 0;
+
+  public LinkedList() {}
+
+  @Override
+  public V get(K key) {
+    // requirement: O(height)
+
+    if () {
+
+    }
+    throw new UnsupportedOperationException("you need to implement get(k)");
+  }
+
+  @Override
+  public boolean containsKey(K key) {
+    // requirement: O(height)
+    throw new UnsupportedOperationException("you need to implement containsKey(k)");
+  }
+
+  @Override
+  public void put(K key, V value) {
+    // requirement: O(height)
+    throw new UnsupportedOperationException("you need to implement put(k,v)");
+  }
+
+  @Override
+  public int size() {
+    // requirement: O(1)
+    throw new UnsupportedOperationException("you need to implement size()");
+  }
+
+  @Override
+  public Deque<K> traverse() {
+    // requirement: O(n)
+    throw new UnsupportedOperationException("you need to implement traverse()");
+  }
+
+  @Override
+  public void remove(K key) {
+    // requirement: O(height)
+    throw new UnsupportedOperationException("implement remove(k) last!");
+  }
+}
