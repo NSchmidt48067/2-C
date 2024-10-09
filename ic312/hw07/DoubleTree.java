d438c62c2b4ad0d5ee82cb57dfcf9b80d3257a09
NSchmidt48067 <vm@usna.edum265646@usna.edu>
2024-10-09 15:18:06 -0400

    new commit

--- ic312/hw07/DoubleTree.java
+++ ic312/hw07/DoubleTree.java
@@ -27,7 +27,7 @@ public class DoubleTree implements AddMax {
       right = r;
     }
     public void update() {
-      balance = (left != null ? left.height: -1) - (right != null ? right.height: -1);
+      balance = (right != null ? right.height: -1) - (left != null ? left.height: -1);
       if (left == null && right == null) {
         height = 0;
       } 
@@ -53,78 +53,64 @@ public class DoubleTree implements AddMax {
       root = new Node(x, null, null);
     }
     else {
-      add(x, root);
+      root = add(x, root);
     }
   }
 
-  public void add(double x, Node cur) {
+  public Node add(double x, Node cur) {
+    // Value is not in the tree, add it
+    if (cur == null) {
+      return new Node(x, null, null);
+    }
+
     int temp = Double.compare(x, cur.data);
 
     // Number is in tree, do nothing
     if (temp == 0) {
-      return;
-    }
-
-    // Key is not in tree, create a new node
-    if (cur.left == null && temp < 0) {
-      cur.left = new Node(x, null, null);
-      
-    }
-    else if (cur.right == null && temp > 0) {
-      cur.right = new Node(x, null, null);
-      
+      return cur;
     }
     // If key is smaller, go left
     // Otherwise go right
     if (temp < 0) {
-      add(x, cur.left);
+      cur.left = add(x, cur.left);
     }
     else if (temp > 0) {
-      add(x, cur.right);
+      cur.right = add(x, cur.right);
     }
-    //Call update to change the height and balance
+    //Update the balance factor and height
     cur.update();
-    // Do nothing if the node is balanced
-    if (isBalanced(cur)) {
+    // If rotations are necessary, perform them
+    cur =  Rebalance(cur);
 
-    }
-    // Rotations
-    else {
-      // Left leaning
-      if (cur.balance < -1) {
-        // Double rotation
-        if (cur.left != null && cur.left.balance == 1) {
-          // Left rotation on left child
-          if (cur.left.left != null) {
-            cur.left = leftRotate(cur.left);
-          }
-          // Right rotation on this node
-          cur = rightRotate(cur);
-        }
-        // Single rotation
-        else {
-          // Right Rotation
-          cur = rightRotate(cur);
-        }
+    return cur;
+
+  }
+
+  public Node Rebalance(Node cur) {
+        //System.out.println(root.left.data);
+
+    
+    // Left leaning
+    if (cur.balance < -1) {
+      // Double rotation
+      if (cur.left != null && cur.left.balance == 1) {
+          cur.left = leftRotate(cur.left);
       }
-      // Right leaning
-      else if (cur.balance > 1) {
-        // Double rotation
-        if (cur.right != null && cur.right.balance == -1) {
-          // Right rotation on right child
-          if (cur.right.right != null) {
-            cur.right = rightRotate(cur.right);
-          }
-          // Left rotation on this node
-          cur = leftRotate(cur);
-        }
-        // Single rotation
-        else {
-          // Left rotation
-          cur = leftRotate(cur);
-        }
-      }
+      // Right rotation on this node
+      return rightRotate(cur);
     }
+    // Right leaning
+    else if (cur.balance > 1) {
+      // Double rotation
+      if (cur.right != null && cur.right.balance == -1) {
+        // Right rotation on right child
+          cur.right = rightRotate(cur.right);
+        }
+        // Left rotation
+        return leftRotate(cur);
+    }
+    // No rotations
+    return cur;
   }
 
   public boolean isBalanced(Node cur) {
@@ -134,22 +120,31 @@ public class DoubleTree implements AddMax {
   //Rotates the tree to the left
   public Node leftRotate(Node oldRoot) {
     Node newRoot = oldRoot.right; 
-    Node middle = newRoot.left;   
+    Node middle = (newRoot == null ? null: newRoot.left);   
     newRoot.left = oldRoot;
     oldRoot.right = middle;
     oldRoot.update();
     newRoot.update();
+
+    if (newRoot.left != null) newRoot.left.update();
+    if (newRoot.right != null) newRoot.right.update();
+
     return newRoot;
   }
   
   //Rotates the tree to the right
   public Node rightRotate(Node oldRoot) {
     Node newRoot = oldRoot.left; 
-    Node middle = newRoot.right;   
+    Node middle = (newRoot == null ? null : newRoot.right);   
     newRoot.right = oldRoot;    
     oldRoot.left = middle;
     oldRoot.update();
     newRoot.update();
+
+    if (newRoot.left != null) newRoot.left.update();
+    if (newRoot.right != null) newRoot.right.update();
+
+
     return newRoot;
   }
 
@@ -159,21 +154,83 @@ public class DoubleTree implements AddMax {
       throw new NoSuchElementException("The tree is empty");
     }
     else {
-      Node temp = removeMax(root);
-      root = temp.left;
-      return temp.data;
+      Node [] temp = new Node[1];
+      root = removeMax(root, temp);
+      return temp[0].data;
     }
   }
 
-  public Node removeMax(Node cur) {
+  public Node removeMax(Node cur, Node[] temp) {
     // Found largest Number
     if (cur.right == null) {
+      temp[0] = cur;
       return cur;
     }
-    cur.right = removeMax(cur.right);
+    cur.right = removeMax(cur.right, temp);
 
     cur.update();
 
     return cur;
+  }
+
+    public Double get(Double x) {
+    //Find value from key
+    return get(root, x);
+  }
+
+    //Helper class for implementation
+  public Double get(Node cur, Double x) {
+    // Key does not exist in tree
+    if (cur == null) {
+      return null;
+    }
+    int temp = Double.compare(x, cur.data);
+
+    // Number is in tree, do nothing
+    if (temp == 0) {
+      return cur.data;
+    }
+    // If key is smaller, go left
+    // Otherwise go right
+    if (temp < 0) {
+      return get(cur.left, x);
+    }
+    else if (temp > 0) {
+      return get(cur.right, x);
+    }
+    else {
+      return cur.data;
+    }
+  }
+
+  public void printAll(Node cur) {
+    if (cur == null) {
+      return;
+    }
+    System.out.println(cur.data);
+    System.out.println(cur.height);
+    System.out.println(cur.balance);
+    System.out.println();
+
+    printAll(cur.left);
+    printAll(cur.right);
+  }
+
+  public void printer() {
+    printAll(root);
+  }
+
+    public static void main(String[] args) {
+    DoubleTree tree = new DoubleTree();
+    tree.add(10.0);
+    tree.add(11.0);
+    tree.add(12.0);
+    tree.add(9.0);
+    tree.add(10.5);
+    tree.add(12.1);
+    tree.add(12.05);
+    tree.printer();
+    //System.out.println(tree.get(12.0));
+
   }
 }
